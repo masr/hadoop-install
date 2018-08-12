11bf1eb6736b12566e289fcb3f8d4d577b5b080f
Suhan Mao <smao@ebay.com>
2018-08-12 21:18:15 +0800

    1

--- config/zookeeper/zookeeper-env.sh
+++ config/zookeeper/zookeeper-env.sh
@@ -4,5 +4,6 @@ export ZOO_LOG_DIR={% hadoop_log_dir %}/zookeeper
 export ZOOPIDFILE={% hadoop_pid_dir %}/zookeeper_server.pid
 export SERVER_JVMFLAGS="-Xms{% zookeeper_heap %} -Xmx{% zookeeper_heap %} -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:${ZOO_LOG_DIR}/zookeeper.gc.`date +'%Y%m%d%H%M'`"
 export ZOO_LOG4J_PROP=INFO,ROLLINGFILE
+export ZOO_LOG_FILE=zookeeper_server-%H.log
 export JAVA=$JAVA_HOME/bin/java
 export JVMFLAGS="-Djute.maxbuffer=5242880"
