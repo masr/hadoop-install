f7f694216cd9ad7ac7dfd961d826502def66cd42
Suhan Mao <smao@ebay.com>
2018-08-15 21:50:31 +0800

    1

--- config/zookeeper/zookeeper-env.sh
+++ config/zookeeper/zookeeper-env.sh
@@ -2,8 +2,15 @@ export JAVA_HOME={% install_base_dir %}/java
 export ZOOKEEPER_HOME={% install_base_dir %}/zookeeper
 export ZOO_LOG_DIR={% hadoop_log_dir %}/zookeeper
 export ZOOPIDFILE={% hadoop_pid_dir %}/zookeeper.pid
-export SERVER_JVMFLAGS="-Xms{% zookeeper_heap %} -Xmx{% zookeeper_heap %} -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:${ZOO_LOG_DIR}/zookeeper.gc.`date +'%Y%m%d%H%M'`"
-export ZOO_LOG4J_PROP=INFO,ROLLINGFILE
-export ZOO_LOG_FILE=zookeeper-%H.log
+export SERVER_JVMFLAGS="-Xms{% zookeeper_heap %} -Xmx{% zookeeper_heap %} -XX:MaxNewSize={%zookeeper_young_heap%} -XX:NewSize={%zookeeper_young_heap%} \
+-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSCompactAtFullCollection \
+-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCApplicationStoppedTime \
+-Xloggc:${ZOO_LOG_DIR}/zookeeper.gc.`date +'%Y%m%d%H%M'` \
+-XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=10 -XX:GCLogFileSize=100M \
+-Dzookeeper.root.logger=${ZOO_LOG4J_PROP:-INFO,ROLLINGFILE} \
+-Dzookeeper.log.dir={% hadoop_log_dir %}/zookeeper \
+-Dzookeeper.log.file=zookeeper-%H.log"
+
+
 export JAVA=$JAVA_HOME/bin/java
 export JVMFLAGS="-Djute.maxbuffer=5242880"
