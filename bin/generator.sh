d39063e50893a291f7ac733bd5c12e2b314ca8f6
Suhan Mao <smao@ebay.com>
2018-08-25 20:08:37 +0800

    1

--- bin/generator.sh
+++ bin/generator.sh
@@ -1,5 +1,5 @@
 basepath=$(cd `dirname $0`;cd ..; pwd)
-source $basepath/.virtualenv/bin/active
+source $basepath/.virtualenv/bin/activate
 export PYTHONPATH=$basepath:$PYTHONPATH
 (cd $basepath; python $basepath/hadoop_install/generator.py $@)
 deactivate
