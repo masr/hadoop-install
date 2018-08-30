5f49389bbd9cfcd0c7bbece4bbdbf930ac3f7ec0
Suhan Mao <smao@ebay.com>
2018-08-30 14:23:13 +0800

    1

--- hadoop_install/processor/java_process.py
+++ hadoop_install/processor/java_process.py
@@ -5,3 +5,6 @@ from hadoop_install.processor.abstract_process import AbstractProcess
 class JavaProcess(AbstractProcess):
     def __init__(self, cluster_name, topology):
         AbstractProcess.__init__(self, cluster_name, SERVICE.JAVA, topology)
+
+    def get_all_parsed_configs(self, group_name):
+        return {}, self.get_merged_basic_configuration_by_group(group_name)
